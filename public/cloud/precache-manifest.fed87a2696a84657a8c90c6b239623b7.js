7385bab008ef5187be6fea791a993a64efac6198
Corentin Ballot <corentin.ballot-ext@socgen.com>
2019-03-28 09:39:35 +0100

    feat: add cloud webapp

---
+++ public/cloud/precache-manifest.fed87a2696a84657a8c90c6b239623b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "61c479ed36e3cd9b1490d2e6bf6adc9e",
+    "url": "/cloud/static/media/iconfont.61c479ed.eot"
+  },
+  {
+    "revision": "5fcda78cc66222d9bc39",
+    "url": "/cloud/static/css/main.8e4a88e9.chunk.css"
+  },
+  {
+    "revision": "6edd116eeee324bf6a85",
+    "url": "/cloud/static/js/runtime~main.827b301a.js"
+  },
+  {
+    "revision": "fdb3fe72572362da1785613a5be2926d",
+    "url": "/cloud/static/media/empty-dir.fdb3fe72.svg"
+  },
+  {
+    "revision": "731af3c2b5a79525c307",
+    "url": "/cloud/static/js/2.6a6f6c69.chunk.js"
+  },
+  {
+    "revision": "9032283ec081393471edbcc1ab85adfd",
+    "url": "/cloud/static/media/network-error.9032283e.svg"
+  },
+  {
+    "revision": "5fcda78cc66222d9bc39",
+    "url": "/cloud/static/js/main.4edaafdb.chunk.js"
+  },
+  {
+    "revision": "2326a17b0719d583a2d9d54c16816c27",
+    "url": "/cloud/static/media/iconfont.2326a17b.ttf"
+  },
+  {
+    "revision": "82107eaec137b05cd5a480867a508590",
+    "url": "/cloud/static/media/iconfont.82107eae.svg"
+  },
+  {
+    "revision": "9141b0332bcf7eb587b4564013af24d7",
+    "url": "/cloud/static/media/iconfont.9141b033.woff"
+  },
+  {
+    "revision": "731af3c2b5a79525c307",
+    "url": "/cloud/static/css/2.b3d5a8ba.chunk.css"
+  },
+  {
+    "revision": "0a188195444188436deb440eea842cce",
+    "url": "/cloud/index.html"
+  }
+];
