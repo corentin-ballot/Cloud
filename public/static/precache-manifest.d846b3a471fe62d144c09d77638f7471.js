aca69973bbf51e0624c5fe2e6359c2721f1e366b
Corentin BALLOT <corentin.ballot@gmail.com>
2020-02-17 18:24:28 +0100

    update front assets

---
+++ public/static/precache-manifest.d846b3a471fe62d144c09d77638f7471.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041ba113d9aa57cdda6bb6013753d8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e58676df69f0226f664",
+    "url": "/static/css/2.f919afc0.chunk.css"
+  },
+  {
+    "revision": "2a2bea5140a1e557cc88",
+    "url": "/static/css/main.8f9d1979.chunk.css"
+  },
+  {
+    "revision": "4e58676df69f0226f664",
+    "url": "/static/js/2.ed010c96.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.ed010c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2bea5140a1e557cc88",
+    "url": "/static/js/main.525b2166.chunk.js"
+  },
+  {
+    "revision": "08d7cdbaaf32d4eee95c",
+    "url": "/static/js/runtime-main.1317442d.js"
+  },
+  {
+    "revision": "2326a17b0719d583a2d9d54c16816c27",
+    "url": "/static/media/iconfont.2326a17b.ttf"
+  },
+  {
+    "revision": "61c479ed36e3cd9b1490d2e6bf6adc9e",
+    "url": "/static/media/iconfont.61c479ed.eot"
+  },
+  {
+    "revision": "82107eaec137b05cd5a480867a508590",
+    "url": "/static/media/iconfont.82107eae.svg"
+  },
+  {
+    "revision": "9141b0332bcf7eb587b4564013af24d7",
+    "url": "/static/media/iconfont.9141b033.woff"
+  },
+  {
+    "revision": "9032283ec081393471edbcc1ab85adfd",
+    "url": "/static/media/network-error.9032283e.svg"
+  }
+]);
