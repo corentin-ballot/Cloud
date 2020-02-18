bebd215c983e221d5a8c6e8e9a8070340157fe8e
Corentin BALLOT <corentin.ballot@gmail.com>
2020-02-18 20:28:08 +0100

    fix authentication redirection

---
+++ src/Security/Authenticator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\BasicAuthenticationEntryPoint;
+
+class Authenticator extends BasicAuthenticationEntryPoint
+{
+    public function __construct(){}
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new RedirectResponse($_SERVER['LOGIN_URL']);
+    }
+}
