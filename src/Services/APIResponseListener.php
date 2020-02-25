ec037b2bc23c84df461e3003963fc6058dd4e9bd
Corentin BALLOT <corentin.ballot-ext@socgen.com>
2020-02-25 09:28:54 +0100

    fix api response header

--- src/Services/APIResponseListener.php
+++ src/Services/APIResponseListener.php
@@ -10,8 +10,12 @@ class APIResponseListener
     {   
         $request = $event->getRequest();
         if(preg_match('/^\/api.*/', $request->getPathInfo())) {
-            // Add 'Access-Control-Allow-Origin: *' header to API routes
-            $event->getResponse()->headers->set('Access-Control-Allow-Origin', "https://".$request->getHost());
+            // WARNING ! Unauthaurized api call are redirected to login (other sub-domain)
+            // As origin is set to null (fetch API), after login redirection (to this sub-domain), 
+            // CORS fails. Set Access-Control-Allow-Origin to null work BUT DEFEATS THE PURPOSE
+            // OF Access-Control-Allow-Origin header.
+            // Source : https://stackoverflow.com/a/40784639
+            $event->getResponse()->headers->set('Access-Control-Allow-Origin', "null");
             $event->getResponse()->headers->set('Access-Control-Allow-Credentials', 'true');
             $event->getResponse()->headers->set('Vary', 'Origin');
         }                                                                                                                                                                                                                         
